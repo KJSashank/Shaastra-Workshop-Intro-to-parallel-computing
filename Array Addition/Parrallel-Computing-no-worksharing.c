ec6cf26290302d4fdfd58223e1451936f1de9457
KJSashank <64363043+KJSashank@users.noreply.github.com>
2021-02-25 12:08:01 +0530

    Create Parrallel-Computing-no-worksharing.c

---
+++ Array Addition/Parrallel-Computing-no-worksharing.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<conio.h>
+#include<stdio.h>
+#inlcude<omp.h>
+
+#define NUM_THREADS 2
+void main()
+{    
+    double a[10],b[10],c[10];
+    int i;
+    for(i=0;i<10;i++){
+      a[i]=i;
+      b[i]=i*i;
+    }
+  #pragma parallel num_threads(NUM_THREADS)
+  {
+    id= omp_get_thread_num;
+    for(i=id;i<10;i=i+NUM_THREADS){
+      c[i]=a[i]+b[i];
+    }
+  }
+}
